267cfb379e34fdc29a10aab0f9ea00b92fef2b84
Samuel Meľuch <samuel.meluch@avast.com>
2022-09-28 22:00:50 +0200

    warning label of value fix

--- src/components/SearchComponent/PlaceInput.jsx
+++ src/components/SearchComponent/PlaceInput.jsx
@@ -54,8 +54,11 @@ export const PlaceInput = ({
     setIsLoading(false);
   }, 300);
 
-  const handleOnInputSelectChange = (options) => {
-    handlePlaceInputChange(options, name);
+  const handleOnInputSelectChange = (values) => {
+    handlePlaceInputChange(
+      values.map((v) => v.value),
+      name
+    );
   };
 
   useEffect(() => {}, [options, isDisabled]);
@@ -66,6 +69,7 @@ export const PlaceInput = ({
       <Select
         data-testid="place-input"
         mode="multiple"
+        labelInValue="true"
         dropdownMatchSelectWidth={false}
         showSearch="true"
         showArrow="true"
@@ -78,14 +82,19 @@ export const PlaceInput = ({
         filterOption={false}
         defaultActiveFirstOption={false}
         onSearch={handleOnInputKeyDown}
-        onChange={(options) => handleOnInputSelectChange(options)}
+        onChange={(values) => handleOnInputSelectChange(values)}
         defaultValue={values}
         style={{ width: "100%" }}
+        optionLabelProp="name"
       >
         {options &&
           options.length &&
           options.map((option) => (
-            <Option key={option.key} value={option.value}>
+            <Option
+              key={option.key}
+              value={option.value}
+              name={option.cityName}
+            >
               <Text strong style={{ display: "block" }}>
                 {option.cityName}
               </Text>
